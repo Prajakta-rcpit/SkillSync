918ff520c7137c51d06a604db240ec6c79ff6229
Prajakta-rcpit <prajaktarcpit@gmail.com>
2025-12-30 01:11:27 +0530

    used clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Outfit } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 export const metadata: Metadata = {
@@ -14,6 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={outfit.className}
@@ -21,5 +23,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-import { Button } from "@/components/ui/button";
+
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
-      <Button>Sign in</Button>
+    <div className="m-10">
+        <SignedOut>
+              <SignInButton/>
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+      
     </div>
   );
 }
