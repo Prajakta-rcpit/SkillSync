fca6307fb37786d5ccf353e9dd35e792b3dd68c7
Prajakta-rcpit <prajaktarcpit@gmail.com>
2026-01-19 15:28:56 +0530

    created new provider

--- components/MeetingModel.tsx
+++ components/MeetingModel.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import React, { useState } from 'react'
+import { Input } from './ui/input';
+import { Button } from './ui/button';
 
-const MeetingModel = () => {
-  return (
-    <div>MeetingModel</div>
-  )
+
+interface MeetingModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  isJoinMeeting: boolean;
+}
+
+const MeetingModel = ({isOpen, onClose, title, isJoinMeeting}: MeetingModalProps) => {
+  const [meetingUrl, setMeetingUrl] = useState("")
+  
+  const createMeeting= async()=>{}
+  const joinMeeting= async()=>{}
+  const handleStart= ()=>{}
+ 
+ 
+  return <Dialog open={isOpen} onOpenChange={onClose}>
+    <DialogContent className="sm:max-w-[425px]">
+      <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+
+      <div className='space-y-4 pt-4'>
+        {isJoinMeeting && (
+            <Input
+              placeholder="Paste meeting link here..."
+              value={meetingUrl}
+              onChange={(e) => setMeetingUrl(e.target.value)}
+            />
+          )}
+
+          <div className="flex justify-end gap-3">
+            <Button variant="outline" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button onClick={handleStart} disabled={isJoinMeeting && !meetingUrl.trim()}>
+              {isJoinMeeting ? "Join Meeting" : "Start Meeting"}
+            </Button>
+          </div>
+
+
+      </div>
+
+
+    </DialogContent>
+  </Dialog>
 }
 
 export default MeetingModel
