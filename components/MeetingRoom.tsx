636ae0191b2a6db541fdec9465e8fdaa0aee34e3
Prajakta-rcpit <prajaktarcpit@gmail.com>
2026-01-26 01:19:57 +0530

    updated meeting room page

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useCallStateHooks } from '@stream-io/video-react-sdk';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+
+
+
 
 const MeetingRoom = () => {
+  const router = useRouter();
+  const [layout, setLayout] = useState<"grid" | "speaker">("speaker")
+  const [showParticipants, setShowParticipants]  = useState(false)
+  const {useCallCallingState} = useCallStateHooks()
+  const callingState = useCallCallingState();
+
   return (
     <div>MeetingRoom</div>
   )
