2ec7cb31ee0bcbe0c8fba932fba5417fe0a11c6d
Prajakta-rcpit <prajaktarcpit@gmail.com>
2026-01-27 12:50:33 +0530

    update page

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,6 +1,8 @@
-import { useCallStateHooks } from '@stream-io/video-react-sdk';
+import { CallingState, useCallStateHooks } from '@stream-io/video-react-sdk';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
+import { LoaderIcon } from 'lucide-react';
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from './ui/resizable';
 
 
 
@@ -12,8 +14,26 @@ const MeetingRoom = () => {
   const {useCallCallingState} = useCallStateHooks()
   const callingState = useCallCallingState();
 
+  if(callingState != CallingState.JOINED){
+    return (
+      <div className='h-96 flex items-center justify-center'>
+        <LoaderIcon className='size-6 animate-spin'/>
+
+      </div>
+    )
+  }
+
   return (
-    <div>MeetingRoom</div>
+    <ResizablePanelGroup direction='horizontal'>
+      <ResizablePanel defaultSize={35} minSize={25} maxSize={100} className='relative'>
+        <h1>Video will go here</h1>
+       </ResizablePanel>
+       <ResizableHandle withHandle/>
+
+       <ResizablePanel defaultSize={65} minSize={25}>
+        <h1>Code editor will go here</h1>
+       </ResizablePanel>
+    </ResizablePanelGroup>
   )
 }
 
