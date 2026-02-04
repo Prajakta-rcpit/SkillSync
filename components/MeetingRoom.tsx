ffe0c84964d6fe94a8b6a81481864364529812b0
Prajakta-rcpit <prajaktarcpit@gmail.com>
2026-02-04 23:41:27 +0530

    update page

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,55 +1,88 @@
-import { CallingState, CallParticipantsList, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react'
-import { LoaderIcon } from 'lucide-react';
-import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from './ui/resizable';
-
+import {
+  CallControls,
+  CallingState,
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+  useCallStateHooks,
+} from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { LayoutListIcon, LoaderIcon } from "lucide-react";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "./ui/resizable";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
 
 const MeetingRoom = () => {
   const router = useRouter();
-  const [layout, setLayout] = useState<"grid" | "speaker">("speaker")
-  const [showParticipants, setShowParticipants]  = useState(false)
-  const {useCallCallingState} = useCallStateHooks()
+  const [layout, setLayout] = useState<"grid" | "speaker">("speaker");
+  const [showParticipants, setShowParticipants] = useState(false);
+  const { useCallCallingState } = useCallStateHooks();
   const callingState = useCallCallingState();
 
-  if(callingState != CallingState.JOINED){
+  if (callingState != CallingState.JOINED) {
     return (
-      <div className='h-96 flex items-center justify-center'>
-        <LoaderIcon className='size-6 animate-spin'/>
-
+      <div className="h-96 flex items-center justify-center">
+        <LoaderIcon className="size-6 animate-spin" />
       </div>
-    )
+    );
   }
 
   return (
-    <ResizablePanelGroup direction='horizontal'>
-      <ResizablePanel defaultSize={35} minSize={25} maxSize={100} className='relative'>
-        <div className='absolute insert-0'>
-        {layout ==='grid' ? <PaginatedGridLayout/>: <SpeakerLayout/>}
-         {showParticipants && (
-              <div className="absolute right-0 top-0 h-full w-[300px] bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-                <CallParticipantsList onClose={() => setShowParticipants(false)} />
-              </div>
-            )}
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel
+        defaultSize={35}
+        minSize={25}
+        maxSize={100}
+        className="relative"
+      >
+        <div className="absolute insert-0">
+          {layout === "grid" ? <PaginatedGridLayout /> : <SpeakerLayout />}
+          {showParticipants && (
+            <div className="absolute right-0 top-0 h-full w-[300px] bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+              <CallParticipantsList
+                onClose={() => setShowParticipants(false)}
+              />
+            </div>
+          )}
         </div>
 
-        <div>
-          <div>
-            <div>
+        <div className="absolute bottom-4 left-0 right-0">
+          <div className="flex flex-col items-center gap-4">
+            <div className="flex items-center gap-2 flex-wrap justify-center px-4">
+              <CallControls onLeave={()=> router.push("/")}/>
+                <div className="flex items-center gap-2">
+                  <DropdownMenu>
+                     <DropdownMenuTrigger asChild>
+                      <Button variant="outline" size="icon" className="size-10">
+                        <LayoutListIcon className="size-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      <DropdownMenuItem onClick={() => setLayout("grid")}>
+                        Grid View
+                      </DropdownMenuItem>
+                      <DropdownMenuItem onClick={() => setLayout("speaker")}>
+                        Speaker View
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+            </div>
           </div>
-          </div>
-          </div>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
 
-
-
-       </ResizablePanel>
-       <ResizableHandle withHandle/>
-
-       <ResizablePanel defaultSize={65} minSize={25}>
+      <ResizablePanel defaultSize={65} minSize={25}>
         <h1>Code editor will go here</h1>
-       </ResizablePanel>
+      </ResizablePanel>
     </ResizablePanelGroup>
-  )
-}
+  );
+};
 
-export default MeetingRoom
+export default MeetingRoom;
